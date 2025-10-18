a6f29fecfc242000dbd4c423a51f68ad448221fd
p5fwh6h5mf-hue <p5fwh6h5mf@privaterelay.appleid.com>
2025-10-18 13:16:28 +0900

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("startButton").addEventListener("click", function() {
-    const frame = document.getElementById("gameFrame");
-    frame.src = "Degrees of lewdity 0.5.5.9.html";
-    document.getElementById("gameContainer").classList.remove("hidden");
-    this.style.display = "none";
-});
